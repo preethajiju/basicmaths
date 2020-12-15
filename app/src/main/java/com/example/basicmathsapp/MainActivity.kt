5315fc2e330d7b0328627edc4c8dfdf6723d6917
preethajiju <preethajiju@gmail.com>
2020-12-16 01:13:41 +0530

    activities added

---
+++ app/src/main/java/com/example/basicmathsapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.basicmathsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun additionevent(view: View) {
+
+    }
+
+    fun subtractionevent(view: View) {
+
+    }
+
+    fun multiplyevent(view: View) {
+
+    }
+
+    fun divisionevent(view: View) {
+
+    }
+}
